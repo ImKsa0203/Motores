1119f5d7fb3f091ec9d0410bb627b9e3d8833b1a
ImKsa0203 <nicocasanova0203@gmail.com>
2024-06-28 12:25:18 -0300

    acomodar, armas, barril, todo idamageable

---
+++ AplicacionDeMotores/Assets/Scripts/Traps/Barrel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour, IDamageable
+{
+    [Header("Stats")]
+    [SerializeField] private int _damage = 100;
+    [Header("Refs")]
+    [SerializeField] private GameObject _explosion;
+
+    public void TakeDamage(int damage)
+    {
+        GameObject explosion = Instantiate(_explosion, transform.position, transform.rotation);
+        explosion.GetComponent<Explosion>().damage = _damage;
+        Destroy(gameObject);
+    }
+}
