7dd123f62bada125048ec341e2ad6d29c7d4e56d
ImKsa0203 <nicocasanova0203@gmail.com>
2024-07-03 19:58:52 -0300

    mini modif

--- AplicacionDeMotores/Assets/Scripts/General/Checkpoint.cs
+++ AplicacionDeMotores/Assets/Scripts/General/Checkpoint.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public int checkpointNum;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() == Player.player)
+        {
+            // Lógica cuando la colisión es con el jugador
+            print("a");
+        }
+    }
 }
